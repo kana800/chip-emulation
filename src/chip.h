be6d467fff09a0b07ee0954fde5e972f9a828de7
kana800 <64830745+kana800@users.noreply.github.com>
2025-04-30 23:46:51 +0530

    bump

--- src/chip.h
+++ src/chip.h
@@ -11,10 +11,12 @@ struct t_chip
 	uint8_t ROM[4096]; // ROM
 	uint8_t RAM[2048]; // RAM
 	uint8_t RAMSTATUS[512]; // STATUS
+	
 	uint8_t ACCUMULATOR;
+	uint8_t CARRYBIT:1;
 
-	uint16_t STACK[3]; // stack ( 3 -> 12-bit register )
-	uint8_t stackpointer;
+	uint16_t STACK[3]; // stack is 3 12-bit register)
+	uint8_t stackpointer:3;
 
 	uint8_t IOPORTS; // 4 bit input (upper) or output ports (lower) 
 };
