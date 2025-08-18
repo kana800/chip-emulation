4da036dd35145eff46278ce4b6f7cc7ac6ecea1a
kana800 <64830745+kana800@users.noreply.github.com>
2025-08-18 23:34:51 +0530

    added some commands

--- 6502/src/instructions.h
+++ 6502/src/instructions.h
@@ -64,4 +64,53 @@ const char* instructions[INS_SIZE] = {
     "TYA" // transfer Y to accumulator 
 };
 
+// addressing mode 
+// A accumulator
+// a absolute
+// x absolute, X-indexed
+// y absolute, Y-indexed
+// # immediate
+// i implied
+// n indirect
+// X X-indexed, indirect
+// Y indirect, Y-indexed
+// r relative
+// z zeropage
+// p zeropage, X-indexed
+// g zeropage, Y-indexed
+const char* instruction_set[256] = {
+    "BRK i", "ORA X", "", "", "", "ORA z", "ASL z", "",
+    "PHP i", "ORA #", "ASL A", "", "", "ORA a", "ASL a", "",
+    "BPL r", "ORA Y", "", "", "", "ORA p", "ASL p", "",
+    "CLC i", "ORA y", "", "", "", "ORA x", "ASL x", "",
+    "JSR a", "AND X", "", "", "BIT z", "AND z", "ROL z", "",
+    "PLP i", "AND #", "ROL A", "", "BIT a", "AND a", "ROL a", "",
+    "BMI r", "AND Y", "", "", "", "AND p", "ROL p", "",
+    "SEC i", "AND y", "", "", "", "AND x", "ROL x", "",
+    "RTI i", "EOR X", "", "", "", "EOR z", "LSR z", "",
+    "PHA i", "EOR #", "LSR A", "", "JMP a", "EOR a", "LSR a", "",
+    "BVC r", "EOR Y", "", "", "", "EOR p", "LSR p", "",
+    "CLI i", "EOR y", "", "", "", "EOR x", "LSR x", "",
+    "RTS i", "ADC X", "", "", "", "ADC z", "ROR z", "",
+    "PLA i", "ADC #", "ROR A", "", "JMP n", "ADC a", "ROR a", "",
+    "BVS r", "ADC Y", "", "", "", "ADC p", "ROR p", "",
+    "SEI i", "ADC y", "", "", "", "ADC x", "ROR x", "",
+    "", "STA X", "", "", "STY z", "STA z", "STX z", "",
+    "DEY i", "", "TXA i", "", "STY a", "STA a", "STX a", "",
+    "BCC r", "STA Y", "", "", "STY p", "STA p", "STX g", "",
+    "TYA i", "STA y", "TXS i", "", "", "STA x", "", "",
+    "LDY #", "LDA X", "LDX #", "", "LDY z", "LDA z", "LDX z", "",
+    "TAY i", "LDA #", "TAX i", "", "LDY a", "LDA a", "LDX a", "",
+    "BCS r", "LDA Y", "", "", "LDY p", "LDA p", "LDX g", "",
+    "CLV i", "LDA y", "TSX i", "", "LDY x", "LDA x", "LDX y", "",
+    "CPY #", "CMP X", "", "", "CPY z", "CMP z", "DEC z", "",
+    "INY i", "CMP #", "DEX i", "", "CPY a", "CMP a", "DEC a", "",
+    "BNE r", "CMP Y", "", "", "", "CMP p", "DEC p", "",
+    "CLD i", "CMP y", "", "", "", "CMP x", "DEC x", "",
+    "CPX #", "SBC X", "", "", "CPX z", "SBC z", "INC z", "",
+    "INX i", "SBC #", "NOP i", "", "CPX a", "SBC a", "INC a", "",
+    "BEQ r", "SBC Y", "", "", "", "SBC p", "INC p", "",
+    "SED i", "SBC y", "", "", "", "SBC x", "INC x", ""
+};
+
 #endif // INSTRUCTION_H
