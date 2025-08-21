312b19fc2bce749b543846c2b5c8ff0e9dfeeb13
kana800 <64830745+kana800@users.noreply.github.com>
2025-08-21 23:36:49 +0530

    completed the breakdown on different addressing modes

--- 6502/src/disassembler.c
+++ 6502/src/disassembler.c
@@ -42,69 +42,105 @@ int main(int argc, char* argv[])
 	uint8_t cycles = 0;
 	uint8_t bytesread = 0;
 	
-	char hexval[2]; 
+	char hexval[3]; 
 	uint8_t hexval_int;
 
-	printf("hex %s\n", buffer);
+	printf("file input (hex dump): %s\n", buffer);
 
 	uint8_t sizeofbyte = 3;
 	
+	// NOTE:experimentation
+	static char twobyte[2];
+	static char fourbyte[4];
+
 	for (int i = 0; i < len;)
 	{
 		memcpy(hexval, buffer + i, 2);
-		hexval_int = strtol(hexval, NULL, 16);
-		const char* instruction = instruction_set[hexval_int];
-		const char address_mode = instruction[4];
+		hexval[2] = '\0';
 
-		printf("%s - %d %s %d %c\n", 
-			hexval, hexval_int, 
-			instruction, strlen(instruction), address_mode);
+		hexval_int = strtol(hexval, NULL, 16);
+		const char* instruction_mode = instruction_set[hexval_int];
+		const char address_mode = instruction_mode[4];
+		
+		char instruction[3]; 
+		memcpy(instruction, instruction_mode, 3);
+
+		uint16_t operand = 0x00;
+		//printf("hex:%s int:%d ins:%s len:%d mode:%c\n", 
+		//	hexval, hexval_int, 
+		//	instruction, 
+		//	strlen(instruction), 
+		//	address_mode);
 
 		switch (address_mode)
 		{
 			case 'A': //accumulator
-				bytesread = 1;
+				printf("%s", instruction);
+				bytesread = 1 * sizeofbyte;
 				break;
 			case 'a': //absolute
+				memcpy(fourbyte, buffer + i + 6, 2);
+				memcpy(fourbyte + 2, buffer + i + 3, 2);
+				fourbyte[4] = '\0';
+				printf("%s $%s\n", instruction, fourbyte);
 				bytesread = 3 * sizeofbyte;
 				break;
 			case 'x': //absolute, X-indexed
-				bytesread = 3;
+				memcpy(twobyte, buffer + i + 3, 2);
+				printf("%s $%s,X\n", instruction, twobyte);
+				bytesread = 3 * sizeofbyte;
 				break;
 			case 'y': //absolute, Y-indexed
+				memcpy(twobyte, buffer + i + 3, 2);
+				printf("%s $%s,Y\n", instruction, twobyte);
+				bytesread = 3 * sizeofbyte;
 				break;
 			case '#': //immediate
+				memcpy(twobyte, buffer + i + 3, 2);
+				printf("%s #$%s\n", instruction, twobyte);
 				bytesread = 2 * sizeofbyte;
 				break;
 			case 'i': //implied
-				bytesread = 1;
-				// byte length is 1 byte
+				printf("%s\n", instruction);
+				bytesread = 1 * sizeofbyte;
 				break;
 			case 'n': //indirect
-				bytesread = 3;
+				memcpy(fourbyte, buffer + i + 5, 4);
+				printf("%s ($%s)\n", instruction, fourbyte);
+				bytesread = 3 * sizeofbyte;
 				break;
 			case 'X': //X-indexed, indirect
-				bytesread = 2;
+				memcpy(twobyte, buffer + i + 3, 2);
+				printf("%s ($%s,X)\n", instruction, twobyte);
+				bytesread = 2 * sizeofbyte;
 				break;
 			case 'Y': //indirect, Y-indexed
-				bytesread = 2;
+				memcpy(twobyte, buffer + i + 3, 2);
+				printf("%s ($%s,Y)\n", instruction, twobyte);
+				bytesread = 2 * sizeofbyte;
 				break;
 			case 'r': //relative
-				bytesread = 2;
+				memcpy(twobyte, buffer + i + 3, 2);
+				printf("%s $%s\n", instruction, twobyte);
+				bytesread = 2 * sizeofbyte;
 				break;
 			case 'z': //zeropage
-				bytesread = 2;
+				memcpy(twobyte, buffer + i + 3, 2);
+				printf("%s $%s\n", instruction, twobyte);
+				bytesread = 2 * sizeofbyte;
 				break;
 			case 'p': //zeropage, X-indexed
-				bytesread = 2;
+				memcpy(twobyte, buffer + i + 3, 2);
+				printf("%s $%s,X\n", instruction, twobyte);
+				bytesread = 2 * sizeofbyte;
 				break;
 			case 'g': //zeropage, Y-indexed
-				bytesread = 2;
+				memcpy(twobyte, buffer + i + 3, 2);
+				printf("%s $%s,Y\n", instruction, twobyte);
+				bytesread = 2 * sizeofbyte;
 				break;
 		}
-
 		i += bytesread;
-
 	}
 	
 	return 0;
